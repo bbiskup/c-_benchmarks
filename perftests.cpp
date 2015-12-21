47de51035d0778c02c160651f9733684bceff32b
Bernhard Biskup <bbiskup@gmx.de>
2015-12-21 10:13:44 +0100

    Accomodate for larger counts; return value from benchmark to force execution

--- perftests.cpp
+++ perftests.cpp
@@ -7,35 +7,66 @@
 using namespace std;
 using namespace boost::chrono;
 
-void benchmark_empty(int count) {
-  for (int i = 0; i < count; ++i) {
+typedef long long count_t;
+
+count_t benchmark_empty(count_t count) {
+  for (count_t i = 0; i < count; ++i) {
     ;
   }
+  return count;
 }
 
-void benchmark_add(int count) {
-  int a = 0;
-  for (int i = 0; i < count; ++i) {
+count_t benchmark_add(count_t count) {
+  count_t a = 0;
+  for (count_t i = 0; i < count; ++i) {
     a += i;
   }
+  return count;
 }
 
-void benchmark_deref_ptr(int count) {
-  int a = 0;
-  int* b = &a;
-  int c;
-  int result;
-  for (int i = 0; i < count; ++i) {
+count_t non_inline_func(count_t x) { return x; }
+
+count_t benchmark_func_call(count_t count) {
+  count_t a = 0;
+  for (count_t i = 0; i < count; ++i) {
+    a += non_inline_func(i);
+  }
+
+  cout << "a: " << a << endl;
+  return count;
+}
+
+inline count_t inline_func(count_t x) { return x; }
+
+count_t benchmark_inline_func_call(count_t count) {
+  count_t a = 0;
+  for (count_t i = 0; i < count; ++i) {
+    a += inline_func(i);
+  }
+
+  cout << "a: " << a << endl;
+  return count;
+}
+
+count_t benchmark_deref_ptr(count_t count) {
+  count_t a = 0;
+  count_t* b = &a;
+  count_t c;
+  count_t result;
+  for (count_t i = 0; i < count; ++i) {
     c = *b;
   }
   result = c;
+  return count;
 }
 
-typedef void (*benchmark_ptr)(int);
+typedef count_t (*benchmark_ptr)(count_t);
 
 map<string, benchmark_ptr> commands = {
     {"empty", benchmark_empty},
     {"add", benchmark_add},
+    {"func_call", benchmark_func_call},
+    {"inline_func_call", benchmark_func_call},
     {"deref_ptr", benchmark_deref_ptr},
 };
 
@@ -46,7 +77,7 @@ int main(int argc, char const* argv[]) {
   }
   auto cmd_name = argv[1];
   auto count_str = argv[2];
-  int count;
+  count_t count;
   istringstream count_strm(count_str);
 
   if (!(count_strm >> count)) {
@@ -61,10 +92,13 @@ int main(int argc, char const* argv[]) {
   }
 
   thread_clock::time_point start = thread_clock::now();
-  cmd_it->second(count);
+  auto result = cmd_it->second(count);
   thread_clock::time_point stop = thread_clock::now();
+
   auto duration = duration_cast<milliseconds>(stop - start).count();
   auto duration_per_call_ns = duration / static_cast<float>(count) * 1000000;
+
+  std::cout << "Result: " << result << endl;
   std::cout << "duration per call: " << duration_per_call_ns << " ns\n";
   std::cout << 1 / duration_per_call_ns * 1000 << " million calls per s\n";
   std::cout << "duration: " << duration << " ms\n";
