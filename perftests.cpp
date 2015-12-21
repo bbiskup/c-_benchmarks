572a6c188ab7e7010332caf21cebbd224c6b6b8d
Bernhard Biskup <bbiskup@gmx.de>
2015-12-21 09:54:48 +0100

    Time measurements; actually apply count

--- perftests.cpp
+++ perftests.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <sstream>
 #include <boost/chrono/thread_clock.hpp>
 
 using namespace std;
 using namespace boost::chrono;
 
-void benchmark_add(int count) { cout << "count: " << count << endl; }
+void benchmark_add(int count) {
+  for (int i = 0; i < count; ++i) {
+    ;;
+  }
+}
 
 typedef void (*benchmark_ptr)(int);
 
@@ -18,6 +23,14 @@ int main(int argc, char const *argv[]) {
     exit(1);
   }
   auto cmd_name = argv[1];
+  auto count_str = argv[2];
+  int count;
+  istringstream count_strm(count_str);
+
+  if (!(count_strm >> count)) {
+    cerr << "Invalid count: '" << count_str << endl;
+    exit(1);
+  }
 
   auto cmd_it = commands.find(cmd_name);
   if (cmd_it == commands.end()) {
@@ -26,9 +39,10 @@ int main(int argc, char const *argv[]) {
   }
 
   thread_clock::time_point start = thread_clock::now();
-  cmd_it->second(10);
+  cmd_it->second(count);
   thread_clock::time_point stop = thread_clock::now();
-  std::cout << "duration: " << duration_cast<milliseconds>(stop - start).count()
-            << " ms\n";
+  auto duration = duration_cast<milliseconds>(stop - start).count();
+  std::cout << "duration per call: " << duration / static_cast<float>(count) * 1000000 << " ns\n";
+  std::cout << "duration: " << duration << " ms\n";
   return 0;
 }
