e5bdeead5afc300c7a755fce72df70a9efa0024c
Bernhard Biskup <bbiskup@gmx.de>
2017-04-01 15:19:38 +0200

    Added benchmarks for vector access

--- cpp_benchmarks.cpp
+++ cpp_benchmarks.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <map>
 #include <string>
 #include <sstream>
@@ -87,6 +88,38 @@ count_t benchmark_read_64k(count_t count) {
   return result;
 }
 
+count_t benchmark_read_64k_vec(count_t count) {
+  const int memSize = 1024 * 64;
+  vector<unsigned char> mem(memSize);
+  for (int j = 0; j < memSize; ++j) {
+    mem[j] = j;
+  }
+
+  int result = 1;
+  for (count_t i = 0; i < count; ++i) {
+    for (int j = 0; j < memSize; ++j) {
+      result &= mem[j];
+    }
+  }
+  return result;
+}
+
+count_t benchmark_read_64k_vec_at(count_t count) {
+  const int memSize = 1024 * 64;
+  vector<unsigned char> mem(memSize);
+  for (int j = 0; j < memSize; ++j) {
+    mem[j] = j;
+  }
+
+  int result = 1;
+  for (count_t i = 0; i < count; ++i) {
+    for (int j = 0; j < memSize; ++j) {
+      result &= mem.at(j);
+    }
+  }
+  return result;
+}
+
 count_t benchmark_try_catch(count_t count) {
   int result = 0;
   for (count_t i = 0; i < count; ++i) {
@@ -158,6 +191,8 @@ map<string, benchmark_ptr> commands = {
     {"deref_ptr", benchmark_deref_ptr},
     {"write_64k", benchmark_write_64k},
     {"read_64k", benchmark_read_64k},
+    {"read_64k_vec", benchmark_read_64k_vec},
+    {"read_64k_vec_at", benchmark_read_64k_vec_at},
     {"try_catch", benchmark_try_catch},
     {"func_ptr", benchmark_func_ptr},
     {"nonvirt_method", benchmark_nonvirt_method},
